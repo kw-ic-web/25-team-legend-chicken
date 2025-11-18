2cc735a56262e3bb9b5f1ba8e9baf346df460dd1
Yooaknow <dbdkfma1228@naver.com>
2025-11-18 20:15:07 +0900

    🎨 학생 사이드바 수정

--- frontend/src/components/layout/student/StudentSidebar.tsx
+++ frontend/src/components/layout/student/StudentSidebar.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback } from "react";
+git add .
 import { useLocation } from "react-router-dom";
 import CommonSidebar from "../CommonSidebar";
 import { joinLecture, getMyLectures } from "../../../api/student";
